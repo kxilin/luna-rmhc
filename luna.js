df8b830b86be031529b79fdd984c236ef18e1c6f
kxlin <kxilin97@gmail.com>
2026-05-20 09:02:31 +1000

    change model

--- luna.js
+++ luna.js
@@ -1,6 +1,6 @@
 const GROQ_API_KEY = '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';
 
-const MODEL = 'llama-3.3-70b-versatile'; // llama-3.1-8b-instant
+const MODEL = 'llama-3.1-8b-instant'; // llama-3.3-70b-versatile
 let conversationHistory = [];
 
 
